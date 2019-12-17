a94df5d868068612e2eb597a53191f23ef0e9544
gabriel <gabriel@nozon.com>
2019-12-17 13:59:36 +0100

    implemented cache manager to handle the searching, managing of cached data and handling of templates fully working filters and search bar implemented basic double click functionality on exr and jpg sequences

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -21,13 +21,9 @@ class AppDialog(QtGui.QWidget):
         # most of the useful accessors are available through the Application class instance
         # it is often handy to keep a reference to this. You can get it via the following method:
         self._app = sgtk.platform.current_bundle()
-        
-        # Get all templates
-        self._output_templates = []
-        for output_profile in self._app.get_setting("templates", []):
-            self._output_templates.append(output_profile)
 
         self._column_names = ColumnNames()
+        self._cache_manager = CacheManager(self, self._app)
 
         self._setup_ui()
         self._fill_projects()
@@ -46,8 +42,8 @@ class AppDialog(QtGui.QWidget):
         refresh_but.setFixedSize(25, 25)
         icon = QtGui.QIcon(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "resources", "refresh.png")))
         refresh_but.setIcon(icon)
-        refresh_but.clicked.connect(self._fill_treewidget)
-
+        refresh_but.clicked.connect(self._refresh)
+        
         upper_bar.addWidget(title_lab)
         upper_bar.addWidget(refresh_but)
         
@@ -59,7 +55,7 @@ class AppDialog(QtGui.QWidget):
         self._project_combo.currentIndexChanged.connect(self._fill_shots)
 
         self._shot_list_widget = QtGui.QListWidget()
-        self._shot_list_widget.itemClicked.connect(self._fill_treewidget)
+        self._shot_list_widget.itemClicked.connect(self._refresh)
 
         filter_widget = QtGui.QLabel('Filters')
         
@@ -95,6 +91,7 @@ class AppDialog(QtGui.QWidget):
         tree_layout = QtGui.QVBoxLayout()
         self._search_bar = QtGui.QLineEdit()
         self._search_bar.setPlaceholderText('Search')
+        self._search_bar.returnPressed.connect(self._fill_treewidget)
 
         self._tree_widget = QtGui.QTreeWidget()
 
@@ -109,6 +106,8 @@ class AppDialog(QtGui.QWidget):
         self._tree_widget.header().setSortIndicatorShown(True)
         self._tree_widget.header().setSectionsClickable(True)
         self._tree_widget.header().setSortIndicator(self._column_names.index_name('modif'), QtCore.Qt.DescendingOrder)
+
+        self._tree_widget.itemDoubleClicked.connect(self._tree_item_double_clicked)
 
         tree_layout.addWidget(self._search_bar)
         tree_layout.addWidget(self._tree_widget)
@@ -144,32 +143,26 @@ class AppDialog(QtGui.QWidget):
 
     def _fill_treewidget(self):
         self._tree_widget.invisibleRootItem().takeChildren()
-        step_selected = self._get_step_filter_selected()
 
-        if self._shot_list_widget.currentItem() and len(step_selected) != 0:
-            caches = []
-            
-            for step in step_selected:
-                ui_fields = {
-                'Shot': self._shot_list_widget.currentItem().text(),
-                'Step': step}
+        # Filters
+        steps = {}
+        for index in range(self._step_list_widget.count()):
+            item = self._step_list_widget.item(index)
+            steps[item.text()] = bool(item.checkState())
+        
+        type_filter = {}
+        for index in range(self._type_list_widget.count()):
+            item = self._type_list_widget.item(index)
+            type_filter[item.text()] = bool(item.checkState())
 
-                for template_dict in self._output_templates:
-                    template = self._app.get_template_by_name(template_dict['cache_template'])
+        # Get caches
+        self._cache_manager.get_caches(self._shot_list_widget.currentItem().text(), steps, type_filter, self._search_bar.text())
 
-                    if self._type_filter(template):
-                        cache_paths = self._app.sgtk.abstract_paths_from_template(template, ui_fields)
-                        for cache_path in cache_paths:
-                            fields = template.get_fields(cache_path)
+        self._tree_widget.header().resizeSections(QtGui.QHeaderView.ResizeToContents)
 
-                            caches.append({'path': cache_path, 'fields': fields})
-
-            for cache in caches:
-                item = TopLevelTreeItem(cache['path'], cache['fields'], self._column_names)
-                self._tree_widget.addTopLevelItem(item)
-                item.set_image()
-
-            self._tree_widget.header().resizeSections(QtGui.QHeaderView.ResizeToContents)
+    def _refresh(self):
+        self._cache_manager.clear_cache()
+        self._fill_treewidget()
 
     def _select_all_filters(self):
         self._step_list_widget.itemChanged.disconnect()
@@ -186,6 +179,34 @@ class AppDialog(QtGui.QWidget):
         self._step_list_widget.itemChanged.connect(self._fill_treewidget)
 
         self._fill_treewidget()
+
+    def _tree_item_double_clicked(self, item, column):
+        path = item.get_path()
+
+        if path[-3:] == 'exr' or path[-3:] == 'jpg':
+            process = QtCore.QProcess(self)
+
+            # run the app
+            system = sys.platform
+            if system == "linux2":
+                program = 'rv'
+            elif system == 'win32':
+                program = 'C:/Program Files/Shotgun/RV-7.2.6/bin/rv.exe'
+            else:
+                msg = "Platform '%s' is not supported." % (system)
+                self._app.log_error(msg)
+                return
+
+            process.startDetached(program, [path])
+            process.close()
+
+    ############################################################################
+    # Public methods
+
+    def add_item_to_tree(self, cache_dict):
+        item = TopLevelTreeItem(cache_dict['path'], cache_dict['fields'], self._column_names)
+        self._tree_widget.addTopLevelItem(item)
+        item.set_image()
 
     ############################################################################
     # Private methods
@@ -231,26 +252,6 @@ class AppDialog(QtGui.QWidget):
 
         self._type_list_widget.setFixedHeight(35)
     
-    def _get_step_filter_selected(self):
-        steps = []
-        for index in range(self._step_list_widget.count()):
-            item = self._step_list_widget.item(index)
-            if item.checkState() == QtCore.Qt.Checked:
-                steps.append(item.text())
-        return steps
-
-    def _type_filter(self, template):
-        if template.definition[-3:] == 'exr':
-            # 2D
-            if self._type_list_widget.item(0).checkState() == QtCore.Qt.Unchecked:
-                return False
-        else:
-            # 3D
-            if self._type_list_widget.item(1).checkState() == QtCore.Qt.Unchecked:
-                return False
-        
-        return True
-
 class ColumnNames():
     def __init__(self):
         self._nice_names = ['Thumbnail', 'Name', 'Version', 'Type', 'Department', 'Last Modified']
@@ -263,9 +264,64 @@ class ColumnNames():
         return self._nice_names
 
 class CacheManager():
-    def __init__(self, app, templates):
+    def __init__(self, dialog, app):
+        self._dialog = dialog
         self._app = app
-        self._templates = templates
+
+        self._2d_cache_dict = {}
+        self._3d_cache_dict = {}
+
+        self._2d_templates = []
+        self._3d_templates = []
+        for output_profile in self._app.get_setting("templates", []):
+            template = self._app.get_template_by_name(output_profile['cache_template'])
+
+            extension = template.definition[-3:]
+            if extension == 'exr' or extension == 'jpg':
+                self._2d_templates.append(template)
+            else:
+                self._3d_templates.append(template)
+
+    def clear_cache(self):
+        self._2d_cache_dict.clear()
+        self._3d_cache_dict.clear()
+
+    def get_caches(self, shot, step_filters, type_filters, search_text):
+        for step, enabled in step_filters.items():
+            if enabled:
+                ui_fields = {
+                'Shot': shot,
+                'Step': step}
+                
+                if type_filters['2D']:
+                    if step in self._2d_cache_dict:
+                        self._add_cached_cache_to_gui(self._2d_cache_dict[step], search_text)
+                    else:
+                        self._2d_cache_dict[step] = self._caches_from_templates(self._2d_templates, ui_fields, search_text)
+                if type_filters['3D']:
+                    if step in self._3d_cache_dict:
+                        self._add_cached_cache_to_gui(self._3d_cache_dict[step], search_text)
+                    else:
+                        self._3d_cache_dict[step] = self._caches_from_templates(self._3d_templates, ui_fields, search_text)
+                    
+    def _caches_from_templates(self, templates, ui_fields, search_text):
+        caches = []
+        for template in templates:
+            cache_paths = self._app.sgtk.abstract_paths_from_template(template, ui_fields)
+            for cache_path in cache_paths:
+                fields = template.get_fields(cache_path)
+                fields['template'] = template
+
+                new_cache_dict = {'path': cache_path, 'fields': fields}
+                caches.append(new_cache_dict)
+                if not search_text or (search_text and search_text in cache_path):
+                    self._dialog.add_item_to_tree(new_cache_dict)
+        return caches
+    
+    def _add_cached_cache_to_gui(self, cache_dict, search_text):
+        for cache in cache_dict:
+            if not search_text or (search_text and search_text in cache['path']):
+                self._dialog.add_item_to_tree(cache)
 
 class TopLevelTreeItem(QtGui.QTreeWidgetItem):
     def __init__(self, path, fields, column_names):
@@ -298,4 +354,7 @@ class TopLevelTreeItem(QtGui.QTreeWidgetItem):
             thumbnail.setPixmap(image)
             self.treeWidget().setItemWidget(self, self._column_names.index_name('thumb'), thumbnail)
             self.treeWidget().header().resizeSections(QtGui.QHeaderView.ResizeToContents)
+
+    def get_path(self):
+        return self._path
 
